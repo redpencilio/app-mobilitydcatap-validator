00966fd2b1834a71a2795bd2a2f08f029b5c6078
Ruben Simons <rubensimons442@gmail.com>
2026-08-20 17:09:36 +0200

    add dcatApVersion property to cogs:job

--- config/resources/task.lisp
+++ config/resources/task.lisp
@@ -5,6 +5,7 @@
                 (:creator :url ,(s-prefix "dct:creator")) ;;Later consider using proper relation in domain.lisp
                 (:status :url ,(s-prefix "adms:status")) ;;Later consider using proper relation in domain.lisp
                 (:operation :url ,(s-prefix "task:operation")) ;;Later consider using proper relation in domain.lisp
+                (:dcat-ap-version :string ,(s-prefix "ext:dcatApVersion"))
                 (:comment :string ,(s-prefix "skos:comment")))
 
   :has-one `((job-error :via ,(s-prefix "task:error")
